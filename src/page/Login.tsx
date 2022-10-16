6ff05f188951b7cd9409375dc31d712fc0c1e272
heybys <byn0174@gmail.com>
2022-10-17 02:57:07 +0900

    update login page

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>login page</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: 16,
+        padding: 128
+      }}
+    >
+      <div style={{ fontSize: 48, fontWeight: 700 }}>로그인</div>
+      <input style={{ width: 256, height: 36 }} value={'id'} />
+      <input style={{ width: 256, height: 36 }} value={'password'} />
+      <button
+        style={{
+          width: 256,
+          height: 36,
+          background: 'skyblue',
+          color: 'white',
+          border: 0,
+          outline: 0
+        }}
+      >
+        로그인
+      </button>
+    </div>
+  );
 };
 
 export default Login;

b73b98b409ed9fcd124fac54a32ff2123879148e
heybys <byn0174@gmail.com>
2023-01-15 08:11:25 +0900

    add menu-board get api to main page

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../../store/hooks';
+import { shopService } from '../../service/shop/shop.service';
 
 const Main = () => {
   const user = useAppSelector((state) => state.user);
+  const [menuBoard, setMenuBoard] = useState();
+
   useEffect(() => {
+    shopService.getMenuBoard(1).then((response) => {
+      setMenuBoard(response.data.payload);
+    });
+
     return () => {};
   }, []);
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 32, padding: 32 }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 32,
+        padding: 32,
+      }}
+    >
       <h3>main</h3>
       <pre>{JSON.stringify(user, undefined, 4)}</pre>
+      <div>{menuBoard && <pre>{JSON.stringify(menuBoard, undefined, 4)}</pre>}</div>
     </div>
   );
 };
